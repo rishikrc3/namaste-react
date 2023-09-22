61a0036c3526a208f0ba1d763d59dc73de0295e1
Rishik Chakravarty <rishikrc3@gmail.com>
2023-09-22 23:42:05 +0530

    Setting up the nav

--- App.js
+++ App.js
@@ -2,7 +2,34 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 //React => this is initially an object and when this gets rendered, this becomes an html element.
-const heading = React.createElement("h1", { id: "heading" }, "namaste react");
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxodBCOx28Vb76OItsEulidWkalURaNu3sMw&usqp=CAU"
+          alt="Not loading"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About us</li>
+          <li>Cart</li>
+          <li>Contact us</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(<AppLayout />);
