7b2e9fce08a4c584b6e70c69a8c6b37cae7f4c14
Rishik Chakravarty <rishikrc3@gmail.com>
2023-09-22 14:34:43 +0530

    Setting up till the last day

--- App.js
+++ App.js
@@ -1,29 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const heading = React.createElement(
-  "h1",
-  {
-    id: "title",
-  },
-  "This is an h1 tag"
-);
-const heading1 = React.createElement(
-  "h2",
-  {
-    id: "title",
-  },
-  "This is an h2 tag"
-);
 
-const container = React.createElement(
-  "div",
-  {
-    id: "container",
-  },
-  [heading, heading1]
-);
-
-// create root using createRoot
+//React => this is initially an object and when this gets rendered, this becomes an html element.
+const heading = React.createElement("h1", { id: "heading" }, "namaste react");
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// passing react element inside root
-root.render(container);
+
+root.render(heading);
